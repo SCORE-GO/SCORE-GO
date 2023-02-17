5c4a96bb4712afd6ac636107d88eac366c96868b
Vaibhav Mashal <110161673+vaibhavmashal@users.noreply.github.com>
2023-02-17 20:40:59 +0530

    insert operation started

--- routes/new_match_router.js
+++ routes/new_match_router.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require("path");
 const client = require('../dbconnect');
 const router = express.Router();
+let user_db;
 
 router.get("", (req, res) => {
     res.sendFile(path.join(__dirname, '../public/views/new-match.html'));
@@ -14,6 +15,16 @@ router.post('/teams', async (req, res) => {
     })
     
 })
+
+router.post("/teams", async (req, res) => {
+    await matches.insertOne(req.body)
+        .catch(() => res.json({ "inserted": false }))
+     user_db = req.body.email.substring(0, req.body.email.indexOf('@')) + "_db"
+    await client.db(user_db).collection('matches').insertMany(require('../public/views/new-match.html'))
+        .then(() => res.json({ "inserted": true }))
+        .catch(() => res.json({ "inserted": false }))
+});
+
 
 
 
