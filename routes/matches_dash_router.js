762252bd421aa8d7ac8928640d349850d960de01
Vaibhav Mashal <110161673+vaibhavmashal@users.noreply.github.com>
2023-03-19 12:49:59 +0530

    one team fetched using exists

--- routes/matches_dash_router.js
+++ routes/matches_dash_router.js
@@ -10,19 +10,26 @@ router.get("", (req, res) => {
     res.sendFile(path.join(__dirname, '../public/views/matches-dash.html'));
 });
 
-// 'router.post('/fetch_matches', async (req, res) => {
-//     let match_info = await client.db(req.body.db).collection("matches").find({}).toArray()
-//     let team_details = await client.db(req.body.db).collection("teams").find({}).toArray()
-//     // let team_details2 = await client.db(req.body.db).collection("teams").find({ name: { $in: [match_info[0].team1, match_info[0].team2] } }, { projection: { _id: 0, name: 1, color: 1 } }).toArray()
-
-//     // let response = {
-//     //     match_title: match_info[0].title,
-//     //     team1: match_info[0].team1,
-//     //     team2: match_info[0].team2,
-//     //     color: team_details[0].color,
-//     // };
-//     // res.json(response);
-
-// })'
+router.post('/fetch_matches', async (req, res) => {
+    // let match_info = await client.db(req.body.db).collection("matches").find({}).toArray()
+     let match_info = await client.db(req.body.db).collection("matches").find({result:{$exists:true}}).toArray()
+     let team_details = await client.db(req.body.db).collection("teams").find({}).toArray() 
+     //let team_details2 = await client.db(req.body.db).collection("teams").find({ name: { $in: [match_info[0].team1, match_info[0].team2] } }, { projection: { _id: 0, name: 1, color: 1 } }).toArray()
+ 
+     // let response = {
+     //     match_title: match_info[0].title,
+     //     team1: match_info[0].team1,
+     //     team2: match_info[0].team2,
+     //     color: team_details[0].color,
+     // };
+     // res.json(response);
+     let response = {
+         match_info: match_info,
+         team_details: team_details
+     }
+     res.json(response);
+ 
+ })
+ 
 
 module.exports = router
